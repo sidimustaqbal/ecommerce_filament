886668c24ff45b5ad585ed8b3ca801bff6a020b0
sidi mustaqbal <sidi.mustaqbal@gmail.com>
2024-08-18 02:01:43 +0700

    implement order stats and tabs above order list

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -14,6 +16,29 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+
+    protected function getFooterWidgets(): array
+    {
+        return [];
+    }
+
+    public function getTabs() : array {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make()->query(fn ($query) => $query->where('status','new')),
+            'proccessing' => Tab::make()->query(fn ($query) => $query->where('status','proccessing')),
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status','shipped')),
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status','delivered')),
+            'cancelled' => Tab::make()->query(fn ($query) => $query->where('status','cancelled')),
         ];
     }
 }
