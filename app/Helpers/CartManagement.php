48a92facfa6720c604e51096f896fedd4986581b
sidi mustaqbal <sidi.mustaqbal@gmail.com>
2024-08-18 22:32:06 +0700

    fix bug quantity not correct when add product from detail product

--- app/Helpers/CartManagement.php
+++ app/Helpers/CartManagement.php
@@ -1,30 +1,32 @@
 <?php
+
 namespace App\Helpers;
 
 use App\Models\Product;
 use Illuminate\Support\Facades\Cookie;
 
-class CartManagement {
+class CartManagement
+{
     // add item to cart
-    static public function addItemToCart($product_id) {
+    static public function addItemToCart($product_id)
+    {
         $cart_items = self::getCartItemsFromCookie();
 
         $existing_item = null;
 
-        foreach($cart_items as $key=>$item) {
-            if($item['product_id']==$product_id) {
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
                 $existing_item  = $key;
                 break;
             }
         }
 
-        if($existing_item !== null) {
+        if ($existing_item !== null) {
             $cart_items[$existing_item]['quantity']++;
             $cart_items[$existing_item]['total_amount'] = $cart_items[$existing_item]['quantity'] * $cart_items[$existing_item]['unit_amount'];
-
         } else {
-            $product = Product::where('id', $product_id)->first(['id','name','price','images']);
-            if($product) {
+            $product = Product::where('id', $product_id)->first(['id', 'name', 'price', 'images']);
+            if ($product) {
                 $cart_items[] = [
                     'product_id' => $product->id,
                     'name' => $product->name,
@@ -41,12 +43,48 @@ class CartManagement {
         return count($cart_items);
     }
 
-    // remove item to cart
-    static public function removeCartItem($product_id) {
+    static public function addItemToCartWithQty($product_id, $quantity = 1)
+    {
         $cart_items = self::getCartItemsFromCookie();
 
-        foreach($cart_items as $key => $item) {
-            if($item['product_id'] == $product_id) {
+        $existing_item = null;
+
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                $existing_item  = $key;
+                break;
+            }
+        }
+
+        if ($existing_item !== null) {
+            $cart_items[$existing_item]['quantity'] = $quantity;
+            $cart_items[$existing_item]['total_amount'] = $cart_items[$existing_item]['quantity'] * $cart_items[$existing_item]['unit_amount'];
+        } else {
+            $product = Product::where('id', $product_id)->first(['id', 'name', 'price', 'images']);
+            if ($product) {
+                $cart_items[] = [
+                    'product_id' => $product->id,
+                    'name' => $product->name,
+                    'image' => $product->images[0],
+                    'quantity' => $quantity,
+                    'unit_amount' => $product->price,
+                    'total_amount' => $product->price
+                ];
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
+
+        return count($cart_items);
+    }
+
+    // remove item to cart
+    static public function removeCartItem($product_id)
+    {
+        $cart_items = self::getCartItemsFromCookie();
+
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
                 unset($cart_items[$key]);
             }
         }
@@ -56,19 +94,22 @@ class CartManagement {
     }
 
     // add cart items to cookie
-    static public function addCartItemsToCookie($cart_items) {
-        Cookie::queue('cart_items', json_encode($cart_items), 60*24*30);        
+    static public function addCartItemsToCookie($cart_items)
+    {
+        Cookie::queue('cart_items', json_encode($cart_items), 60 * 24 * 30);
     }
 
     // clear cart items from cookie
-    static public function clearCartItems() {
+    static public function clearCartItems()
+    {
         Cookie::queue(Cookie::forget('cart_items'));
     }
 
     // get all cart items from cookie
-    static public function getCartItemsFromCookie() {
+    static public function getCartItemsFromCookie()
+    {
         $cart_items = json_decode(Cookie::get('cart_items'), true);
-        if(!$cart_items) {
+        if (!$cart_items) {
             $cart_items = [];
         }
 
@@ -76,11 +117,12 @@ class CartManagement {
     }
 
     // increment item quantity
-    static public function incementQuantityToCartItem($product_id) {
+    static public function incementQuantityToCartItem($product_id)
+    {
         $cart_items = self::getCartItemsFromCookie();
 
-        foreach($cart_items as $key=>$item) {
-            if($item['product_id']==$product_id) {
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
                 $cart_items[$key]['quantity']++;
                 $cart_items[$key]['total_amount'] = $cart_items[$key]['quantity'] * $cart_items[$key]['unit_amount'];
             }
@@ -91,12 +133,13 @@ class CartManagement {
     }
 
     // decrement item quantity
-    static public function decrementQuantityToCartItem($product_id) {
+    static public function decrementQuantityToCartItem($product_id)
+    {
         $cart_items = self::getCartItemsFromCookie();
 
-        foreach($cart_items as $key=>$item) {
-            if($item['product_id']==$product_id) {
-                if($cart_items[$key]['quantity'] > 1) {
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                if ($cart_items[$key]['quantity'] > 1) {
                     $cart_items[$key]['quantity']--;
                     $cart_items[$key]['total_amount'] = $cart_items[$key]['quantity'] * $cart_items[$key]['unit_amount'];
                 }
@@ -108,7 +151,8 @@ class CartManagement {
     }
 
     // calculate grand total
-    static public function calculateGrandTotal($items) {
+    static public function calculateGrandTotal($items)
+    {
         return array_sum(array_column($items, 'total_amount'));
     }
 }
