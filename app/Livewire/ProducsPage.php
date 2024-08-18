7f97ae2c3574f7e78c3da2fc5c13d6cde769bba0
sidi mustaqbal <sidi.mustaqbal@gmail.com>
2024-08-18 12:52:19 +0700

    implement dynamic products list and product detail page

--- app/Livewire/ProducsPage.php
+++ app/Livewire/ProducsPage.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
+
+#[Title('Products | E Commerce App')]
 
 class ProducsPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.producs-page');
+        $productQuery = Product::query()->where('is_active',1);
+        return view('livewire.producs-page', [
+            'products' => $productQuery->paginate(3),
+            'brands'=>Brand::where('is_active',1)->get(['id','name','slug']),
+            'categories'=>Category::where('is_active',1)->get(['id','name','slug']),
+        ]);
     }
 }
--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Products Detail | E Commerce App')]
 
 class ProductDetailPage extends Component
 {
+    public $slug;
+    
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+    
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail()
+        ]);
     }
 }
