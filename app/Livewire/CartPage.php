fdd1e84a8305c941819f2da44203f408ff2003e8
sidi mustaqbal <sidi.mustaqbal@gmail.com>
2024-08-20 05:28:36 +0700

    implement cart page

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,35 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Products | E Commerce App')]
 
 class CartPage extends Component
 {
+    public $cart_items = [];
+    public $grand_total = 0;
+
+    public function mount() {
+        $this->cart_items = CartManagement::getCartItemsFromCookie();
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+    public function removeItem($product_id) {
+        $this->cart_items = CartManagement::removeCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+        $this->dispatch('update-cart-count', total_count: count($this->cart_items))->to(Navbar::class);
+    }
+    public function increaseQty($product_id){
+        $this->cart_items = CartManagement::incementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+    public function decreaseQty($product_id){
+        $this->cart_items = CartManagement::decrementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
     public function render()
     {
         return view('livewire.cart-page');
