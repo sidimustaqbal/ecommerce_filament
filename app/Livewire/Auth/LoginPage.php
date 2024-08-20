eff32af93ed87e96f40c15d46d85d1496844da76
sidi mustaqbal <sidi.mustaqbal@gmail.com>
2024-08-20 13:11:53 +0700

    implement login page

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class LoginPage extends Component
 {
+    public $email;
+    public $password;
+
+    public function save() {
+        $this->validate([
+            'email' => 'required|email|max:255|exists:users,email',
+            'password'=> 'required|min:6|max:255'
+        ]);
+
+        if(Auth::attempt(['email'=>$this->email,'password'=>$this->password])) {
+            session()->flash('error','Invalid Credentials');
+
+            return;
+        }
+
+        return redirect()->intended();
+    }
     public function render()
     {
         return view('livewire.auth.login-page');
--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -75,7 +75,7 @@
                             <span class="font-semibold">{{ Number::currency($grand_total, 'IDR') }}</span>
                         </div>
                         @if ($cart_items)
-                            <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+                            <a href="/checkout" class="block text-center bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</a>
                         @endif
                     </div>
                 </div>
