a33bcf765adca2d0a139e17033b2ff85ca3605db
sidi mustaqbal <sidi.mustaqbal@gmail.com>
2024-08-20 10:30:23 +0700

    implement register function

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Livewire\MyOrdersPage;
 use App\Livewire\ProducsPage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\SuccessPage;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
@@ -21,14 +22,23 @@ Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProducsPage::class);
 Route::get('/cart', CartPage::class);
 Route::get('/products/{slug}', ProductDetailPage::class);
-Route::get('/checkout', CheckoutPage::class);
-Route::get('/my-orders', MyOrdersPage::class);
-Route::get('/my-orders/{order}', MyOrdersDetailPage::class);
 
-Route::get('/login', LoginPage::class);
-Route::get('/register', RegisterPage::class);
-Route::get('/forgot', ForgotPasswordPage::class);
-Route::get('/reset', ResetPasswordPage::class);
+Route::middleware('guest')->group(function() {
+    Route::get('/login', LoginPage::class);
+    Route::get('/register', RegisterPage::class);
+    Route::get('/forgot', ForgotPasswordPage::class);
+    Route::get('/reset', ResetPasswordPage::class);
+});
 
-Route::get('/success', SuccessPage::class);
-Route::get('/cancel', CancelPage::class);
+Route::middleware('auth')->group(function() {
+    Route::get('/logout', function() {
+        Auth::logout();
+        return redirect()->to('/');
+    });
+    Route::get('/checkout', CheckoutPage::class);
+    Route::get('/my-orders', MyOrdersPage::class);
+    Route::get('/my-orders/{order}', MyOrdersDetailPage::class);
+
+    Route::get('/success', SuccessPage::class);
+    Route::get('/cancel', CancelPage::class);
+});
